8a1a2a2d1a27c61ffe0d6551857e756d1530aa30
mpguchii <mpguchii@gmail.com>
2026-07-06 20:55:31 -0300

    Update title to LAST WAR TTS, add generated logo, and use local category overview icons in sidebar

--- app.js
+++ app.js
@@ -1,43 +1,24 @@
 // LAST WAR: RESEARCH PLANNER - JS CONTROLLER
 
-// Mapping categories to friendly icons
-const ICONS = {
-  development: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" /><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" /></svg>`,
-  economy: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>`,
-  hero: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" /></svg>`,
-  units: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" /></svg>`,
-  squad: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" /></svg>`,
-  alliance: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z" /></svg>`,
-  special: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" /></svg>`,
-  siege: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 17a2 2 0 11-4 0 2 2 0 014 0zM19 17a2 2 0 11-4 0 2 2 0 014 0z" /><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16V6a1 1 0 00-1-1H4a1 1 0 00-1 1v10a1 1 0 001 1h1m8-1a1 1 0 01-1 1H9m4-1V8a1 1 0 011-1h2.586a1 1 0 01.707.293l2.414 2.414a1 1 0 01.293.707V16a1 1 0 01-1 1h-1m-6-1a1 1 0 001 1h1M5 17h2" /></svg>`,
-  defense: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z" /></svg>`,
-  tank: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 13a1 1 0 011 1v2a2 2 0 01-2 2H4a2 2 0 01-2-2v-2a1 1 0 011-1m18 0H3m18 0a2 2 0 00-2-2h-3.586a1 1 0 01-.707-.293l-1.414-1.414A1 1 0 0012.586 7H7a2 2 0 00-2 2v4m14-6H9m4 0v-3h-2v3" /></svg>`,
-  missile: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" /></svg>`,
-  aircraft: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" /></svg>`,
-  oil: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z" /></svg>`,
-  tactical: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15a3 3 0 11-6 0 3 3 0 016 0z" /><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 1.5V3m0 18v1.5M1.5 12H3m18 0h1.5M19.8 19.8l-1.05-1.05M5.25 5.25l-1.05-1.05m15.6 0l-1.05 1.05M5.25 18.75l-1.05 1.05" /></svg>`,
-  t10: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.907c.969 0 1.371 1.24.588 1.81l-3.97 2.883a1 1 0 00-.364 1.118l1.52 4.674c.3.922-.755 1.688-1.538 1.118l-3.971-2.883a1 1 0 00-1.18 0l-3.97 2.883c-.783.57-1.838-.197-1.538-1.118l1.52-4.674a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h4.907a1 1 0 00.95-.69l1.519-4.674z" /></svg>`
-};
-
 const CATEGORIES_MAPPING = {
-  "development": { title: "Development", icon: ICONS.development },
-  "economy": { title: "Economy", icon: ICONS.economy },
-  "hero": { title: "Hero", icon: ICONS.hero },
-  "units": { title: "Units", icon: ICONS.units },
-  "squad-1": { title: "Squad 1", icon: ICONS.squad },
-  "squad-2": { title: "Squad 2", icon: ICONS.squad },
-  "squad-3": { title: "Squad 3", icon: ICONS.squad },
-  "squad-4": { title: "Squad 4", icon: ICONS.squad },
-  "alliance-duel": { title: "Alliance Duel", icon: ICONS.alliance },
-  "special-forces": { title: "Special Forces", icon: ICONS.special },
-  "siege-to-seize": { title: "Siege to Seize", icon: ICONS.siege },
-  "defense-fortifications": { title: "Defense Fortifications", icon: ICONS.defense },
-  "tank-mastery": { title: "Tank Mastery", icon: ICONS.tank },
-  "missile-mastery": { title: "Missile Mastery", icon: ICONS.missile },
-  "aircraft-mastery": { title: "Aircraft Mastery", icon: ICONS.aircraft },
-  "the-age-of-oil": { title: "The Age of Oil", icon: ICONS.oil },
-  "tactical-weapon": { title: "Tactical Weapon", icon: ICONS.tactical },
-  "t10-special-forces": { title: "T10 Special Forces", icon: ICONS.t10 }
+  "development": { title: "Development", icon: "images/tech/overview/development.png" },
+  "economy": { title: "Economy", icon: "images/tech/overview/economy-new.png" },
+  "hero": { title: "Hero", icon: "images/tech/overview/hero-new.png" },
+  "units": { title: "Units", icon: "images/tech/overview/units-new.png" },
+  "squad-1": { title: "Squad 1", icon: "images/tech/overview/squad-1-new.png" },
+  "squad-2": { title: "Squad 2", icon: "images/tech/overview/squad-2-new.png" },
+  "squad-3": { title: "Squad 3", icon: "images/tech/overview/squad-3-new.png" },
+  "squad-4": { title: "Squad 4", icon: "images/tech/overview/squad-4-new.png" },
+  "alliance-duel": { title: "Alliance Duel", icon: "images/tech/overview/alliance-duel-new.png" },
+  "special-forces": { title: "Special Forces", icon: "images/tech/overview/special-forces-new.png" },
+  "siege-to-seize": { title: "Siege to Seize", icon: "images/tech/overview/siege-to-seize-new.png" },
+  "defense-fortifications": { title: "Defense Fortifications", icon: "images/tech/overview/defense-fortifications.png" },
+  "tank-mastery": { title: "Tank Mastery", icon: "images/tech/overview/tank-mastery-new.png" },
+  "missile-mastery": { title: "Missile Mastery", icon: "images/tech/overview/missile-mastery-new.png" },
+  "aircraft-mastery": { title: "Aircraft Mastery", icon: "images/tech/overview/aircraft-mastery-new.png" },
+  "the-age-of-oil": { title: "The Age of Oil", icon: "images/tech/overview/the-age-of-oil-new.png" },
+  "tactical-weapon": { title: "Tactical Weapon", icon: "images/tech/overview/tactical-weapon-new.png" },
+  "t10-special-forces": { title: "T10 Special Forces", icon: "images/tech/overview/special-forces-new.png" }
 };
 
 const CATEGORIES_ORDER = Object.keys(CATEGORIES_MAPPING);
@@ -236,7 +217,9 @@ function renderCategories() {
     btn.setAttribute("data-category", cat);
     
     btn.innerHTML = `
-      <div class="cat-icon-wrapper">${config.icon}</div>
+      <div class="cat-icon-wrapper">
+        <img src="${config.icon}" alt="${config.title}" class="cat-icon-img">
+      </div>
       <div class="cat-text-info">
         <span class="cat-name">${config.title}</span>
         <div class="cat-progress-wrapper">
@@ -288,7 +271,7 @@ function renderTechsGrid() {
       card.setAttribute("data-key", key);
       
       // Select dynamic category-styled icon
-      const iconSvg = CATEGORIES_MAPPING[activeCategory].icon;
+      const catIcon = CATEGORIES_MAPPING[activeCategory].icon;
       
       // Build levels indicator (dots or bar depending on max_level count)
       let levelsIndicatorHtml = "";
@@ -311,8 +294,7 @@ function renderTechsGrid() {
         <span class="max-badge">MAX</span>
         <div class="tech-card-top">
           <div class="tech-icon-container">
-            <img src="${tech.image}" alt="${tech.name}" class="tech-icon-img" onerror="this.style.display='none'; this.nextElementSibling.style.display='block';">
-            <div style="display:none; width:100%; height:100%;" class="fallback-svg-icon">${iconSvg}</div>
+            <img src="${tech.image}" alt="${tech.name}" class="tech-icon-img" onerror="this.src='${catIcon}';">
           </div>
           <div class="tech-name-wrapper">
             <span class="tech-card-name">${tech.name}</span>
